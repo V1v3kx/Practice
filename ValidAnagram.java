a71323685f463134978c8e57f050b9767f04663f
Vivek <vivekreddy.n.k2003@gmail.com>
2026-06-11 11:05:27 +0530

    Add files via upload

---
+++ ValidAnagram.java
@@ -0,0 +1,35 @@
+package dailyPractice;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+	
+	public static void main(String[] args) {
+		ValidAnagram a=new ValidAnagram();
+		System.out.println(a.isAnagram("anagram", "naagram"));
+		System.out.println(a.isAnagram("rat", "tar"));
+	}
+	
+	
+	
+	public boolean isAnagram(String s, String t) {
+        boolean anagram=true;
+        int sl=s.length();
+        int tl=t.length();
+        if(sl!=tl){
+            anagram=false;
+            return anagram;
+        }
+        char[] sarr=s.toCharArray();
+        char[] tarr=t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        for(int i=0;i<sarr.length;i++){
+            if(sarr[i]!=tarr[i]){
+                anagram=false;
+                return anagram;
+            }
+        }
+        return anagram;
+    }
+}
