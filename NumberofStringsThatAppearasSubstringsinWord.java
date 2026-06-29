f63511a7b896f11d647465de7a8901e3ff7dbf27
Vivek <vivekreddy.n.k2003@gmail.com>
2026-06-29 11:16:48 +0530

    Add files via upload

---
+++ NumberofStringsThatAppearasSubstringsinWord.java
@@ -0,0 +1,22 @@
+package dailyPractice;
+
+
+public class NumberofStringsThatAppearasSubstringsinWord {
+	public static void main(String[] args) {
+		Solution51 s=new Solution51();
+		String[] patterns= {"a","b","c"};
+		String word="aaaaabbbbb";
+		System.out.println(s.numOfStrings(patterns,word));
+	}
+}
+class Solution51 {
+    public int numOfStrings(String[] patterns, String word) {
+        int count=0;
+        for(String pattern:patterns) {
+        		if(word.contains(pattern)) {
+        			count++;
+        		}
+        }
+        return count;
+    }
+}
